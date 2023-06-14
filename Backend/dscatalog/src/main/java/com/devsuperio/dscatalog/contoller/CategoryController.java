bdfd1c7c57349f16f566e281b9b8d121dc99d5df
Ivanildo Ribeiro <Ivanribeiro011@hotmail.com>
2023-06-14 11:16:16 -0300

    Post category

--- Backend/dscatalog/src/main/java/com/devsuperio/dscatalog/contoller/CategoryController.java
+++ Backend/dscatalog/src/main/java/com/devsuperio/dscatalog/contoller/CategoryController.java
@@ -6,11 +6,10 @@ import com.devsuperio.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,5 +25,11 @@ public class CategoryController {
     @GetMapping("{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable("id") Long id){
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(service.insert(dto));
     }
 }
--- Backend/dscatalog/src/main/java/com/devsuperio/dscatalog/services/CategoryService.java
+++ Backend/dscatalog/src/main/java/com/devsuperio/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperio.dscatalog.services;
 
 import com.devsuperio.dscatalog.dtos.CategoryDTO;
+import com.devsuperio.dscatalog.entity.Category;
 import com.devsuperio.dscatalog.exceptions.EntityNotFoundException;
 import com.devsuperio.dscatalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,11 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
         return new CategoryDTO(repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Entity not found")));
+    }
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto){
+        Category category = new Category();
+        category.setName(dto.getName());
+        return new CategoryDTO(repository.save(category));
     }
 }
